f745c931f3161c9c08f94ca140ed2695c09214db
tridoof <sam445544@gmail.com>
2015-07-28 20:40:10 -0700

    Add global variables to be used throughout the application

---
+++ app/src/main/java/com/samuel/cachelocator/Constants.java
@@ -0,0 +1,12 @@
+package com.samuel.cachelocator;
+
+/**
+ * Created by Samuel on 7/22/2015.
+ */
+public class Constants {
+    //Class
+
+    //Fields
+    public static final float METERS_PER_FEET = 0.3048f;
+    public static final int METERS_PER_KILOMETER = 1000;
+}
